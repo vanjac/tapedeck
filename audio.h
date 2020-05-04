da2f74eccae367497ad521ffe2fee3aa45f1d07a
jace v <vantjac@gmail.com>
2020-05-03 22:48:07 -0700

    restore previous buffer size

--- audio.h
+++ audio.h
@@ -1,9 +1,9 @@
 #include <pulse/simple.h>
 
 // affects playback latency
-#define PLAYBACK_BUFFER_SIZE 512
+#define PLAYBACK_BUFFER_SIZE 4096
 // affects record latency
-#define RECORD_BUFFER_SIZE 512
+#define RECORD_BUFFER_SIZE 2048
 
 int audio_open(void);
 void audio_close(void);
